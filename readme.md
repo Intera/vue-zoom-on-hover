9e94251a63e4db6f4fdda30e74059012cbf78236
Julian Kalbhenn <jkalbhenn@intera.de>
2018-04-23 09:55:06 +0000

    add updated javascript version with js and css files instead of a .vue file. support for events on load and resize. readme update

--- readme.md
+++ readme.md
@@ -8,10 +8,10 @@ responsive image with zoomed image on hover.
 this [vue.js](https://vuejs.org/) component displays an image with the width of the parent element and on hover shows the full image or a scaled image in the image area
 
 # files
-`zoom-on-hover.vue` is the single-file component. a non-single-file version can be extracted from `demo/main.html`
+`zoomOnHover.js` registers the vue component and defines zoomOnHover, a variable for the component configuration object. `zoomOnHover.css` contains the needed styles
 
 # usage
-minimal example (with the div as example parent container)
+minimal example (with an example <div> as parent container)
 ```html
 <div style="width:400px">
   <zoom-on-hover img-normal="image.jpg"></zoom-on-hover>
@@ -20,13 +20,16 @@ minimal example (with the div as example parent container)
 
 all options
 ```html
-<zoom-on-hover img-normal="image.jpg" img-zoom="bigger-image.jpg" :scale="1.5" :disabled="true"></zoom-on-hover>
+<zoom-on-hover img-normal="image.jpg" img-zoom="bigger-image.jpg" :scale="1.5" :disabled="true"
+  @loaded="onload" @resized="onresize"></zoom-on-hover>
 ```
 
 # features
 * enabled/disabled property
 * custom scale for zoomed image
 * optionally a separate zoom image
+* event when all images loaded
+* event when images resized (responsive css, etc)
 
 # caveats
-if the parent container is bigger than the source image, the normal image stretches to the size of the parent container but the zoom image will have the normal image scaled width (will be smaller for example)
+if the parent container is bigger than the source image, the normal image stretches to the size of the parent container but the zoom image will have the original width (will be smaller for example)
---
+++ zoomOnHover.js
@@ -0,0 +1,101 @@
+// this component displays an image with the width of the parent element and on hover shows
+// the full image or a scaled image in the image area.
+// features: activate/deactivate method, active/inactive on load, scale parameter, separate zoom image,
+// event when all images loaded, event when images resized (responsive css, etc)
+
+function pageOffset(el) {
+	// -> {x: number, y: number}
+	// get the left and top offset of a dom block element
+	var rect = el.getBoundingClientRect(),
+		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	return {
+		y: rect.top + scrollTop,
+		x: rect.left + scrollLeft
+	}
+}
+
+var zoomOnHover = {
+	props: ["imgNormal", "imgZoom", "scale", "disabled"],
+	template: "<div class=\"zoom-on-hover\" @mousemove=\"move\" @mouseenter=\"zoom\" @mouseleave=\"unzoom\">" +
+		"<img class=\"normal\" ref=\"normal\" :src=\"imgNormal\"/>" +
+		"<img class=\"zoom\" ref=\"zoom\" :src=\"imgZoom || imgNormal\"/>" +
+		"</div>",
+	data: function () {
+		return {
+			scaleFactor: 1,
+			resizeCheckInterval: null
+		}
+	},
+	methods: {
+		zoom: function () {
+			if (this.disabled) return
+			this.$refs.zoom.style.opacity = 1
+			this.$refs.normal.style.opacity = 0
+		},
+		unzoom: function () {
+			if (this.disabled) return
+			this.$refs.zoom.style.opacity = 0
+			this.$refs.normal.style.opacity = 1
+		},
+		move: function (event) {
+			if (this.disabled) return
+			var offset = pageOffset(this.$el)
+			var zoom = this.$refs.zoom
+			var normal = this.$refs.normal
+			var relativeX = event.clientX - offset.x + window.pageXOffset
+			var relativeY = event.clientY - offset.y + window.pageYOffset
+			var normalFactorX = relativeX / normal.offsetWidth
+			var normalFactorY = relativeY / normal.offsetHeight
+			var x = normalFactorX * (zoom.offsetWidth * this.scaleFactor - normal.offsetWidth)
+			var y = normalFactorY * (zoom.offsetHeight * this.scaleFactor - normal.offsetHeight)
+			zoom.style.left = -x + "px"
+			zoom.style.top = -y + "px"
+		},
+		initEventLoaded: function () {
+			// emit the "loaded" event if all images have been loaded
+			var promises = [this.$refs.zoom, this.$refs.normal].map(function (image) {
+				return new Promise(function (resolve, reject) {
+					image.addEventListener("load", resolve)
+					image.addEventListener("error", reject)
+				})
+			})
+			var component = this
+			Promise.all(promises).then(function () {
+				component.$emit("loaded")
+			})
+		},
+		initEventResized: function () {
+			var normal = this.$refs.normal
+			var previousWidth = normal.offsetWidth
+			var previousHeight = normal.offsetHeight
+			var component = this
+			this.resizeCheckInterval = setInterval(function () {
+				if ((previousWidth != normal.offsetWidth) || (previousHeight != normal.offsetHeight)) {
+					previousWidth = normal.offsetWidth
+					previousHeight = normal.offsetHeight
+					component.$emit("resized", {
+						width: normal.width,
+						height: normal.height,
+						fullWidth: normal.naturalWidth,
+						fullHeight: normal.naturalHeight
+					})
+				}
+			}, 1000)
+		}
+	},
+	mounted: function () {
+		if (this.$props.scale) {
+			this.scaleFactor = parseInt(this.$props.scale)
+			this.$refs.zoom.style.transform = "scale(" + this.scaleFactor + ")"
+		}
+		this.initEventLoaded()
+		this.initEventResized()
+	},
+	updated: function () {
+		this.initEventLoaded()
+	},
+	beforeDestroy: function () {
+		this.resizeCheckInterval && clearInterval(this.resizeCheckInterval)
+	}
+}
